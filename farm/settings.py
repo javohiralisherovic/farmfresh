bd76d6b06b5e411efced7c36d931732036229893
javohiralisherovic <105439156+javohiralisherovic@users.noreply.github.com>
2022-11-25 10:41:04 +0500

    root
    root comment

--- farm/settings.py
+++ farm/settings.py
@@ -154,7 +154,7 @@ MODELTRANSLATION_CUSTOM_FIELDS = ('MyField', 'MyOtherField',)
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+# STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
