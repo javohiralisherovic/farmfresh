31837e83e49e36937bf535bb3afe79b491fc365f
javohiralisherovic <105439156+javohiralisherovic@users.noreply.github.com>
2022-11-28 15:35:42 +0500

    aa

--- farm/settings.py
+++ farm/settings.py
@@ -15,6 +15,8 @@ import os
 import django_heroku
 import dj_database_url
 from decouple import config
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -48,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware", 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
