922f83158655a367368f8526a2945cf513676e10
javohiralisherovic <105439156+javohiralisherovic@users.noreply.github.com>
2022-11-28 15:17:21 +0500

    sS

--- farm/settings.py
+++ farm/settings.py
@@ -169,10 +169,10 @@ MODELTRANSLATION_CUSTOM_FIELDS = ('MyField', 'MyOtherField',)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
