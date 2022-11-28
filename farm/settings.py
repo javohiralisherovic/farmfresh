bf7efb8ebd871a2197ab610d8f1a2c23f92b2370
javohiralisherovic <105439156+javohiralisherovic@users.noreply.github.com>
2022-11-28 12:57:17 +0500

    ss
    ss

--- farm/settings.py
+++ farm/settings.py
@@ -82,22 +82,23 @@ WSGI_APPLICATION = 'farm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'farm.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'farm',
-        'USER': 'farmuser',
-        'PASSWORD': 'a',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'farm.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'farm',
+#         'USER': 'farmuser',
+#         'PASSWORD': 'a',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
